b9c0bb634ce37d3e9cc765ac13efb6e0a5ff2eee
xuedong1126 <xuexiaodongloveni@foxmail.com>
2019-12-02 13:48:47 +0800

    this is test

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="main">
+      <HellowWrold/>
+    </div>
+    <div class="footer">
+     <router-link>dddd</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HellowWrold from '../components/HelloWorld'
+export default {
+  name: 'home',
+  components: {
+    HellowWrold
+  }
+}
+</script>
